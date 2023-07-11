06afecbfe9651f98c38eb260e84e7ac87e748491
Muhammad Ivan Wiryawan <ivanchalik@student.ub.ac.id>
2023-07-11 14:37:47 +0700

    Deleted unrelated files & Commented lines for queries

--- script_bot.py
+++ script_bot.py
@@ -134,6 +134,7 @@ def is_authenticated(user_id: int):
 def authenticate_user(func):
     @wraps(func)
     def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_status=None):
+        # ![Query Autentikasi User & Admin]
         # Check if the user is authenticated
         auth_status = is_authenticated(update.effective_user.id)
         if auth_status[0]:
@@ -218,6 +219,7 @@ async def input_data(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_st
 @authenticate_admin
 async def input_satuan(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_status=None):
     file = context.bot.get_file(update.message.document.file_id)
+    # ![Query Input Satuan]
     await update.message.reply_text('Input satuan berhasil.')
     context.chat_data['in_conversation'] = False
     return ConversationHandler.END
@@ -225,6 +227,7 @@ async def input_satuan(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_
 @authenticate_admin
 async def input_pangkas(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_status=None):
     file = context.bot.get_file(update.message.document.file_id)
+    # ![Query Input Truncate]
     await update.message.reply_text('Pangkas dan input berhasil.')
     context.chat_data['in_conversation'] = False
     return ConversationHandler.END
@@ -270,11 +273,12 @@ async def proses_tambah_user(update: Update, context: ContextTypes.DEFAULT_TYPE,
     userid = decoded_token['user_id']
     nama = decoded_token['nama']
 
-    if userid == '1139987918':
+    if userid == '1139987918': #[Query Cari User Berdasarkan ID]
         await update.message.reply_text('User ID yang dimasukkan telah terdaftar. Proses tambah user dibatalkan.')
         context.chat_data['in_conversation'] = False
         return ConversationHandler.END
 
+    # ![Query Tambah User]
     await update.message.reply_text(f'Berhasil menambahkan user berikut.\nUser ID: {userid}\nNama: {nama}')
     context.chat_data['in_conversation'] = False
     return ConversationHandler.END
@@ -286,16 +290,17 @@ async def proses_tambah_admin(update: Update, context: ContextTypes.DEFAULT_TYPE
     userid = decoded_token['user_id']
     nama = decoded_token['nama']
 
-    if userid == '1139987918':
+    if userid == '1139987918': #[Query Cari User Berdasarkan ID]
         await update.message.reply_text('User ID yang dimasukkan telah terdaftar. Proses tambah user dibatalkan.')
         context.chat_data['in_conversation'] = False
         return ConversationHandler.END
 
+    # ![Query Tambah User]
     await update.message.reply_text(f'Berhasil menambahkan user berikut.\nUser ID: {userid}\nNama: {nama}')
     context.chat_data['in_conversation'] = False
     return ConversationHandler.END
 
-### Hapus User (TODO)
+### Hapus User
 @authenticate_admin
 async def hapus_user(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_status=None):
     query = update.callback_query
@@ -307,6 +312,7 @@ async def hapus_user(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_st
 
     context.chat_data['in_conversation'] = True
 
+    # ![Query Nama & ID User]
     test_list = ['M. Ivan Wiryawan', 'User 1', 'User 2', 'User 3', 'User 4', 'User 5', 'User 6']
     keyboard = [[InlineKeyboardButton(name, callback_data = 'hapus_' + name.replace(' ', '_'))] for name in test_list]
 
@@ -335,9 +341,10 @@ async def proses_hapus_user(update: Update, context: ContextTypes.DEFAULT_TYPE, 
     await context.bot.edit_message_reply_markup(chat_id=query.message.chat_id, message_id=query.message.message_id, reply_markup=None)
 
     if button_pressed == 'konfirmasi_hapus_ya':
-        if context.chat_data['nama_user'] == 'M. Ivan Wiryawan':
+        if context.chat_data['nama_user'] == 'M. Ivan Wiryawan': # ![Query Jumlah Admin]
             reply = 'Tidak bisa menghapuskan user admin terakhir, silahkan tambahkan admin baru terlebih dahulu. Proses penghapusan user dibatalkan.'
         else:
+            # ![Query Delete User]
             reply = context.chat_data['nama_user'] + ' berhasil dihapuskan dari daftar user.'
     elif button_pressed == 'konfirmasi_hapus_tidak':
         reply = 'Proses penghapusan user dibatalkan.'
@@ -365,6 +372,7 @@ async def proses_peroleh_lokasi(update: Update, context: ContextTypes.DEFAULT_TY
     if len(text) == 8 and text.isalnum():
 
         if text.upper() == '20BAT001':
+            # ![Query Data Site]
             latitude = -7.88267
             longitude = 112.527
             await update.message.reply_text('Site_ID_Tenant: 20BAT001\nTenant: INDOSAT\nAlamat: Jl. Dewi Sartika Atas 110-104, Temas, Kec. Batu, Kota Batu, Jawa Timur 65315\nKoordinat Site: -7.88267,112.527')
@@ -388,6 +396,7 @@ async def peroleh_lokasi_func(update: Update, context: ContextTypes.DEFAULT_TYPE
         if len(argument) == 8 and argument.isalnum():
 
             if argument.upper() == '20BAT001':
+                # ![Query Data Site]
                 latitude = -7.88267
                 longitude = 112.527
                 await update.message.reply_text('Site_ID_Tenant: 20BAT001\nTenant: INDOSAT\nAlamat: Jl. Dewi Sartika Atas 110-104, Temas, Kec. Batu, Kota Batu, Jawa Timur 65315\nKoordinat Site: -7.88267,112.527')
@@ -426,6 +435,7 @@ async def peroleh_nama_2(update: Update, context: ContextTypes.DEFAULT_TYPE, aut
 @authenticate_user
 async def proses_peroleh_nama(update: Update, context: ContextTypes.DEFAULT_TYPE, auth_status=None):
     dist = update.message.text
+    # ![Query Berdasarkan Jarak & Radius]
 
     if dist.lstrip('-').replace(',', '').replace('.', '').isnumeric():
         if float(dist) <= 0:
@@ -518,151 +528,7 @@ async def get_token_process(update: Update, context: ContextTypes.DEFAULT_TYPE, 
     await update.message.reply_text(f'Token anda adalah: {token}')
     context.chat_data['in_conversation'] = False
     return ConversationHandler.END
-# # Function that creates a message containing a list of all the oders
-# def create_message_select_query(ans):
-#     text = ''
-#     for i in ans:
-#         id = i[0]
-#         product = i[1]
-#         quantity = i[2]
-#         creation_date = i[3]
-#         text += '<b>'+ str(id) +'</b> | ' + '<b>'+ str(product) +'</b> | ' + '<b>'+ str(quantity)+'</b> | ' + '<b>'+ str(creation_date)+'</b>\n'
-#     message = '<b>Received 📖 </b> Information about orders:\n\n'+text
-#     return message
-#
-# ### SELECT COMMAND
-# @client.on(events.NewMessage(pattern='(?i)/select'))
-# async def select(event):
-#     try:
-#         # Get the sender of the message
-#         sender = await event.get_sender()
-#         SENDER = sender.id
-#         # Execute the query and get all (*) the oders
-#         crsr.execute('SELECT * FROM orders')
-#         res = crsr.fetchall() # fetch all the results
-#         # If there is at least 1 row selected, print a message with the list of all the oders
-#         # The message is created using the function defined above
-#         if(res):
-#             text = create_message_select_query(res)
-#             await client.send_message(SENDER, text, parse_mode='html')
-#         # Otherwhise, print a default text
-#         else:
-#             text = 'No orders found inside the database.'
-#             await client.send_message(SENDER, text, parse_mode='html')
-#
-#     except Exception as e:
-#         print(e)
-#         await client.send_message(SENDER, 'Something Wrong happened... Check your code!', parse_mode='html')
-#         return
-#
-#
-#
-# ### UPDATE COMMAND
-# @client.on(events.NewMessage(pattern='(?i)/update'))
-# async def update(event):
-#     try:
-#         # Get the sender
-#         sender = await event.get_sender()
-#         SENDER = sender.id
-#
-#         # Get the text of the user AFTER the /update command and convert it to a list (we are splitting by the SPACE ' ' simbol)
-#         list_of_words = event.message.text.split(' ')
-#         id = int(list_of_words[1]) # second (1) item is the id
-#         new_product = list_of_words[2] # third (2) item is the product
-#         new_quantity = list_of_words[3] # fourth (3) item is the quantity
-#         dt_string = datetime.now().strftime('%d/%m/%Y') # We create the new date
-#
-#         # create the tuple with all the params interted by the user
-#         params = (id, new_product, new_quantity, dt_string, id)
-#
-#         # Create the UPDATE query, we are updating the product with a specific id so we must put the WHERE clause
-#         sql_command='UPDATE orders SET id=%s, product=%s, quantity=%s, LAST_EDIT=%s WHERE id =%s'
-#         crsr.execute(sql_command, params) # Execute the query
-#         conn.commit() # Commit the changes
-#
-#         # If at least 1 row is affected by the query we send a specific message
-#         if crsr.rowcount < 1:
-#             text = 'Order with id {} is not present'.format(id)
-#             await client.send_message(SENDER, text, parse_mode='html')
-#         else:
-#             text = 'Order with id {} correctly updated'.format(id)
-#             await client.send_message(SENDER, text, parse_mode='html')
-#
-#     except Exception as e:
-#         print(e)
-#         await client.send_message(SENDER, 'Something Wrong happened... Check your code!', parse_mode='html')
-#         return
-#
-#
-#
-# @client.on(events.NewMessage(pattern='(?i)/delete'))
-# async def delete(event):
-#     try:
-#         # Get the sender
-#         sender = await event.get_sender()
-#         SENDER = sender.id
-#
-#         #/ delete 1
-#
-#         # get list of words inserted by the user
-#         list_of_words = event.message.text.split(' ')
-#         id = list_of_words[1] # The second (1) element is the id
-#
-#         # Crete the DELETE query passing the id as a parameter
-#         sql_command = 'DELETE FROM orders WHERE id = (%s);'
-#
-#         # ans here will be the number of rows affected by the delete
-#         ans = crsr.execute(sql_command, (id,))
-#         conn.commit()
-#
-#         # If at least 1 row is affected by the query we send a specific message
-#         if ans < 1:
-#             text = 'Order with id {} is not present'.format(id)
-#             await client.send_message(SENDER, text, parse_mode='html')
-#         else:
-#             text = 'Order with id {} was correctly deleted'.format(id)
-#             await client.send_message(SENDER, text, parse_mode='html')
-#
-#     except Exception as e:
-#         print(e)
-#         await client.send_message(SENDER, 'Something Wrong happened... Check your code!', parse_mode='html')
-#         return
-#
-#
-#
-# # Create database function
-# def create_database(query):
-#     try:
-#         crsr_mysql.execute(query)
-#         print('Database created successfully')
-#     except Exception as e:
-#         print(f'WARNING: '{e}'')
-#
+
 ##### MAIN
 if __name__ == '__main__':
     main()
-#     try:
-#         print('Initializing Database...')
-#         conn_mysql = MySQLdb.connect( host=HOSTNAME, user=USERNAME, passwd=PASSWORD )
-#         crsr_mysql = conn_mysql.cursor()
-#
-#         query = 'CREATE DATABASE '+str(DATABASE)
-#         create_database(query)
-#         conn = MySQLdb.connect( host=HOSTNAME, user=USERNAME, passwd=PASSWORD, db=DATABASE )
-#         crsr = conn.cursor()
-#
-#         # Command that creates the 'oders' table
-#         sql_command = '''CREATE TABLE IF NOT EXISTS orders (
-#             id INTEGER PRIMARY KEY AUTO_INCREMENT,
-#             product VARCHAR(200),
-#             quantity INT(10),
-#             LAST_EDIT VARCHAR(100));'''
-#
-#         crsr.execute(sql_command)
-#         print('All tables are ready')
-#
-#         print('Bot Started...')
-#         client.run_until_disconnected()
-#
-#     except Exception as error:
-#         print('Cause: {}'.format(error))
